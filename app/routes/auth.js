7e26863654d687cc13072aa517dc1f072ee923eb
Talha Ansari <talhajansari@hotmail.com>
2016-12-12 00:03:13 -0500

    Giving structure to the return response

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,52 +5,32 @@ var authhelper = require('./auth_helper');
 module.exports = function (router) {
 
   handleAuthToken = function (req, res) {
+    
     var user = req.user;
-    if (!user) {
-      return res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
-      if (!user.isValidPassword(req.body.password)) {
-        return res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-        // create a token
-        var secret_key_common = req.app.get('superSecret');
+    
+    // create a token
+    var secret_key_common = req.app.get('superSecret');
 
-        var token = jwt.sign(user,  secret_key_common, {
-          expiresIn: 1440*60 // expires in 24 hours
-        });
-        console.log(token);
-        // return the information including token as JSON
-        return res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }
-    }
+    var token = jwt.sign(user,  secret_key_common, {
+      expiresIn: 1440*60 // expires in 24 hours
+    });
+    console.log(token);
+    // return the information including token as JSON
+    return res.json({
+      success: true,
+      error: null,
+      message: 'Enjoy your token!',
+      data: {token: token}
+    });
+      
   };
-
-  // passportAuthenticate = function(req, res, next) {
-  //   passport.authenticate('local', {session: false}, function(err, user, info) {
-  //     if (err) { return next(err); } //error exception
-  //    // user will be set to false, if not authenticated
-  //     if (!user) {
-  //         return res.json({ success: false, message: 'Authentication failed. User not found.' });
-  //     } else {
-  //         // if user authenticated maintain the session
-  //         req.logIn(user, function() {
-  //           // do whatever here on successful login
-  //           handleAuthToken(req, res);
-  //         })
-  //     }
-  //   });
-  // };
 
   router.post('/login', function(req, res, next) {
     passport.authenticate('local', function(err, user, info) {
       if (err) { return next(err); } //error exception
      // user will be set to false, if not authenticated
       if (!user) {
-          return res.json({ success: false, message: 'Authentication failed. User not found.' });
+          return res.json({ success: false, error: null, message: 'Authentication failed. User not found.', data: null });
       } 
       // if user authenticated maintain the session
       req.logIn(user, {session: false}, function(err) {
